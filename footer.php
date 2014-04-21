2a52d85552a168a737b3941cf3f8808e44eeaa22
Vasilis van Gemert <github@vasilis.nl>
2014-04-21 11:54:21 +0200

    Updated the footer

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
         <p>Follow this blog <a href="http://nerd.vasilis.nl/feed/">via plain RSS</a> or
             <a href="http://www.feedly.com/home#subscription/feed%2Fhttp%3A%2F%2Fnerd.vasilis.nl%2Ffeed%2F">directly via Feedly</a>.
             Read more <a href="http://nerd.vasilis.nl/about/">about Vasilis</a>, why I <a href="http://lovenonsense.com/">Love Nonsense</a>, what I do in <a href="http://wasstra.at/">De Wasstraat</a>.
-            Or stay up to date with <a href="http://dailynerd.nl/">(!Daily) Nerd</a>, via <a href="https://twitter.com/vasilis">Twitter</a>, or through <a href="https://alpha.app.net/vasilis">App.net</a>.
-            You might also like my <a href="https://secure.flickr.com/photos/vasilisvg/">Flickr</a> photos. None of the above? Maybe you prefer <a href="http://1nothingaday.com/">Nothing</a>?
+            Or stay up to date with <a href="http://dailynerd.nl/">(!Daily) Nerd</a>, or via <a href="https://twitter.com/vasilis">Twitter</a>.
+            You might also like my <a href="http://vasilis.nl/voto/">photos</a>. None of the above? Maybe you prefer <a href="http://amorvacui.com/">Nothing</a>?
             Or maybe you're looking for <a href="http://www.webfaction.com/services/hosting?affiliate=vasilis">Hosting</a>? <small>&copy; 2009 - 2014 <a href="http://vasilis.nl/">Vasilis van Gemert</a>. <a href="http://nerd.vasilis.nl/feed/">RSS</a></small></p>
     </nav>
     <div></div>
