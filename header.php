ec910553f288f2d30f1b7edf243228c531b52982
Vasilis van Gemert <github@vasilis.nl>
2014-10-07 20:40:06 +0200

    Switch to one single layout. This is too hard

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-$files = array();
+/* $files = array();
 $patterns_dir = "./wp-content/themes/nerd-ratio/css";
 
 //If not, show all images
@@ -20,7 +20,7 @@ while (false !== ($file = readdir($handle))):
     endif;
 endwhile;
 
-$amount = (count($files))-1;
+$amount = (count($files))-1; */
 
 ?>
 <!doctype html>
@@ -32,14 +32,14 @@ $amount = (count($files))-1;
 <link rel="stylesheet" href="http://fonts.typotheque.com/WF-023273-006956.css" type="text/css" />
 <?php
 // get the date of the stylesheet
-$css = $files[rand(0,$amount)];
+/* $css = $files[rand(0,$amount)];
 $cssFile = './wp-content/themes/nerd-ratio/css/'.$css;
 $cssTime = date ("U", filemtime($cssFile));
 $cssName = explode('.css',$css);
-$css = $cssName[0].'.'.$cssTime.'.css';
+$css = $cssName[0].'.'.$cssTime.'.css'; */
 
 ?>
-<link rel="stylesheet" href="/wp-content/themes/nerd-ratio/css/<?php echo $css; ?>">
+<link rel="stylesheet" href="/wp-content/themes/nerd-ratio/css/trent-walton.css">
 <link rel="alternate" type="application/rss+xml" title="Nerd feed" href="/feed/">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <meta name="application-name" content="Nerd.">
