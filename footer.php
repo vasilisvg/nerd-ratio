46a4d4e96de7a0ccdfaba192a8183eb459297f97
Vasilis van Gemert <github@vasilis.nl>
2014-11-01 10:12:29 +0100

    Added email address

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <p>Follow this blog <a href="http://nerd.vasilis.nl/feed/">via plain RSS</a> or
             <a href="http://www.feedly.com/home#subscription/feed%2Fhttp%3A%2F%2Fnerd.vasilis.nl%2Ffeed%2F">directly via Feedly</a>.
             Read more <a href="http://nerd.vasilis.nl/about/">about Vasilis</a>, why I <a href="http://lovenonsense.com/">Love Nonsense</a>, what I do in <a href="http://wasstra.at/">De Wasstraat</a>.
-            Or stay up to date with <a href="http://dailynerd.nl/">(!Daily) Nerd</a>, or via <a href="https://twitter.com/vasilis">Twitter</a>.
+            Or contact me via <a href="https://twitter.com/vasilis">Twitter</a>. You can email me at nerd[at]vasilis.nl. 
             You might also like my <a href="http://vasilis.nl/voto/">photos</a>. None of the above? Maybe you prefer <a href="http://amorvacui.com/">Nothing</a>?
             Or maybe you're looking for <a href="http://www.webfaction.com/services/hosting?affiliate=vasilis">Hosting</a>? <small>&copy; 2009 - 2014 <a href="http://vasilis.nl/">Vasilis van Gemert</a>. <a href="http://nerd.vasilis.nl/feed/">RSS</a></small></p>
     </nav>
